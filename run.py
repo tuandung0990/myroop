70c64545851859d164269e9c0b2754b6023f4b6f
tuandung0990 <142638808+tuandung0990@users.noreply.github.com>
2025-04-12 14:44:11 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python3
+from roop.core import face_swap_main
+import argparse
 
-from roop import core
+def main():
+    # Tạo đối tượng parser cho dòng lệnh
+    parser = argparse.ArgumentParser(description="Faceswap with Roop")
+    parser.add_argument('--source', required=True, help="Đường dẫn tới ảnh nguồn")
+    parser.add_argument('--target', required=True, help="Đường dẫn tới ảnh đích")
+    parser.add_argument('--output', required=True, help="Đường dẫn lưu ảnh đầu ra")
+    
+    args = parser.parse_args()
+    
+    # Gọi hàm faceswap từ roop.core
+    face_swap_main(args.source, args.target, args.output)
 
 if __name__ == '__main__':
-    core.run()
+    main()
